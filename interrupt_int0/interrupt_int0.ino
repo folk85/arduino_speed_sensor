70f1b2308a71a05aeb8eae903c0c24e7632cf573
Sergey Medvedev <medvsn@gmail.com>
2017-11-02 13:30:15 +0300

    Set debug mode to check model

--- interrupt_int0/interrupt_int0.ino
+++ interrupt_int0/interrupt_int0.ino
@@ -7,7 +7,8 @@
 int istate;
 int ii,iio;
 int tt1, tt2;
-unsigned long t1, t2;
+unsigned long t1, t2,t0,t_disp;
+unsigned long tau_disp = 500000; // time of period to redraw display
 int pin = 2;
 uint8_t pDisp0;    // point
 
@@ -18,10 +19,12 @@ void setup(void)
 {
 //    pinMode(2, INPUT);
 //    pinMode(13, OUTPUT);
-    ii = 0; // set initial Value of counter to 0.
-    iio = 0; // set initail Value of old-counter to 0
+    ii = 8; // set initial Value of counter to 0.
+    iio = 7; // set initail Value of old-counter to 0
     tt1 = 0;
     tt2 = 0;
+
+    t_disp = 0; //Set initial time to 0;
 
     DDRB |= 0x31; // output PB0(pin8)-Display, PB4(pin12) and PB5(pin13);
     PORTB &=~0x31;
@@ -30,7 +33,9 @@ void setup(void)
 
     pDisp0 = 8;        // pointer to number of PD4 (pin04) defined as output for Display
 
-    sei();                    // Enable global interrupts
+    Serial.begin(9600);
+    
+//    sei();                    // Enable global interrupts
 //    digitalWrite(2, HIGH);    // Enable pullup resistor
 //    EIMSK |= (1 << INT0);     // Enable external interrupt INT0
 //    EICRA |= (1 << ISC01);    // Trigger INT0 on falling edge
@@ -45,21 +50,43 @@ void loop(void)
   istate = PIND & PN;
   
   if ( istate == PN){
-//    t1 = micros();
-    t1 = millis();
+    t1 = micros();
+//    t1 = millis();
     PORTB |= 0x20;
     while (istate == PN){
       istate = PIND & PN;
     }
-    if ( istate != PN){
-//      t2 = micros();
-      t2 = millis();
-      if (ii< 10) ii++;
-      tt1 = (int)(t2-t1);
-      tt2 = (tt2 * iio + tt1) / ii;
-      if (iio < 9) iio = ii - 1;
-      displayInt(pDisp0,tt1);
-      delay(20);
+    if ( istate == 0x00){
+      t2 = micros();
+//      t2 = millis();
+//      if (ii< 10) iio = ii;
+//      if (ii< 10) ii += 1;
+      t0 = t2 - t1;
+      tt1 = (int)t0;
+      if ( tt2 == 0 ) tt2 = tt1;
+//      tt2 = (tt2<<1) + (tt2<<2) + tt1 + tt2;
+//      tt2 >>= 3;
+//      if (t0 > 1) {
+        tt2 *= iio;
+        tt2 += tt1;
+        tt2 /= ii;
+//        tt2 = (tt2 * iio + tt1) / ii;
+          Serial.print("  CurLong:");
+          Serial.print(t0);
+          Serial.print("  Cur:");
+          Serial.print(tt1);
+          Serial.print("  Ave: ");
+          Serial.println(tt2);
+        if (t2 - t_disp > tau_disp) {
+          displayInt(pDisp0,tt2);
+//          Serial.print("  ii: ");
+//          Serial.print(ii);
+//          Serial.print("  iio: ");
+//          Serial.print(iio);
+          t_disp = t2;
+        }
+//      }
+      delay(10);
       PORTB &= ~0x20;
     }
   }
